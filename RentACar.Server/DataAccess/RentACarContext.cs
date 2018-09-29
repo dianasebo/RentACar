a2a096311de093ae3bf29a816419b4e8a7190662
Ameteo <stan.ameteo@gmail.com>
2018-09-29 20:58:14 +0300

    Added users to database context

--- RentACar.Server/DataAccess/RentACarContext.cs
+++ RentACar.Server/DataAccess/RentACarContext.cs
@@ -4,6 +4,8 @@ using RentACar.Shared.Models;
 
 namespace RentACar.Server.DataAccess {
     public class RentACarContext : DbContext {
+        public virtual DbSet<User> users { get; set; }
+
         protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
             if (!optionsBuilder.IsConfigured)
                 optionsBuilder.UseMySql ("server=localhost;database=rent_a_car;user=root;password=root");

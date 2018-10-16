0c0a07653b0b73980c872074cf038d3d24718869
Diana <sebodiana@gmail.com>
2018-10-16 16:25:46 +0300

    Added ...stuff.

---
+++ RentACar.Client/UserInput.cs
@@ -0,0 +1,19 @@
+﻿using RentACar.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentACar.Client
+{
+    public class UserInput
+    {
+        public string Brand { get; set; } = null;
+        public string Model { get; set; } = null;
+        public EngineType? Engine { get; set; } = null;
+        public string Year { get; set; } = null;
+        public string City { get; set; } = null;
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
--- RentACar.Server/Controllers/CarController.cs
+++ RentACar.Server/Controllers/CarController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RentACar.Client;
 using RentACar.Server.DataAccess;
 using RentACar.Shared.Models;
 
@@ -55,112 +56,18 @@ namespace RentACar.Server.Controllers
         public IEnumerable<string> GetAllCities() => carDAO.GetAllCities();
 
 
-        //---------- functions used for autocompletion ----------
 
-        [HttpGet]
-        [Route("api/Cars/Brands/Random")]
-        public string GetRandomBrand() => carDAO.GetRandomBrand();
+        [HttpPost]
+        [Route("api/Cars/Search")]
+        public IEnumerable<Car> Search([FromBody] UserInput UserInput)
+        {
+            IEnumerable<Car> cars = carDAO.GetAllCars();
+            cars = !string.IsNullOrEmpty(UserInput.Brand) ? cars.Where(c => c.Brand.Equals(UserInput.Brand)) : cars;
+            cars = !string.IsNullOrEmpty(UserInput.Model) ? cars.Where(c => c.Model.Equals(UserInput.Model)) : cars;
+            cars = UserInput.Engine != null ? cars.Where(c => c.Engine.Equals(UserInput.Engine)) : cars;
+            cars = UserInput.Year != null ? cars.Where(c => c.Year.Equals(UserInput.Year)) : cars;
 
-        [HttpGet]
-        [Route("api/Cars/{brand}/Models/Random")]
-        public string GetRandomModelForBrand(string brand) => carDAO.GetRandomModelForBrand(brand);
-
-        [HttpGet]
-        [Route("api/Cars/{model}/Years/Random")]
-        public int GetRandomYearForModel(string model) => carDAO.GetRandomYearForModel(model);
-
-        [HttpGet]
-        [Route("api/Cars/Years/Random")]
-        public int GetRandomYear() => carDAO.GetRandomYear();
-
-
-        //---------- functions used for updating filters ----------
-
-        //---------- brand selected ----------
-        [HttpGet]
-        [Route("api/Cars/{brand}/ModelsForBrand")]
-        public IEnumerable<string> GetModelsForBrand(string brand) => carDAO.GetModelsForBrand(brand);
-
-        [HttpGet]
-        [Route("api/Cars/{brand}/EnginesForBrand")]
-        public IEnumerable<EngineType> GetEngineTypesForBrand(string brand) => carDAO.GetEnginesForBrand(brand);
-        
-        [HttpGet]
-        [Route("api/Cars/{brand}/YearsForBrand")]
-        public IEnumerable<int> GetYearsForBrand(string brand) => carDAO.GetYearsForBrand(brand);
-
-        [HttpGet]
-        [Route("api/Cars/{brand}/CitiesForBrand")]
-        public IEnumerable<string> GetCitiesForBrand(string brand) => carDAO.GetCitiesForBrand(brand);
-
-
-        //---------- model selected ----------
-        [HttpGet]
-        [Route("api/Cars/{model}/BrandForModel")]
-        public IEnumerable<string> GetBrandForModel(string model) => carDAO.GetBrandForModel(model);
-
-        [HttpGet]
-        [Route("api/Cars/{model}/EnginesForModel")]
-        public IEnumerable<EngineType> GetEngineTypesForModel(string model) => carDAO.GetEnginesForModel(model);
-
-        [HttpGet]
-        [Route("api/Cars/{model}/YearsForModel")]
-        public IEnumerable<int> GetYearsForModel(string model) => carDAO.GetYearsForModel(model);
-
-        [HttpGet]
-        [Route("api/Cars/{model}/CitiesForModel")]
-        public IEnumerable<string> GetCitiesForModel(string model) => carDAO.GetCitiesForModel(model);
-
-
-        //---------- engine type selected ----------
-        [HttpGet]
-        [Route("api/Cars/{engine}/BrandsForEngine")]
-        public IEnumerable<string> GetBrandsForEngine(EngineType engine) => carDAO.GetBrandsForEngine(engine);
-
-        [HttpGet]
-        [Route("api/Cars/{engine}/ModelsForEngine")]
-        public IEnumerable<string> GetModelsForEngine(EngineType engine) => carDAO.GetModelsForEngine(engine);
-
-        [HttpGet]
-        [Route("api/Cars/{engine}/YearsForEngine")]
-        public IEnumerable<int> GetYearsForEngine(EngineType engine) => carDAO.GetYearsForEngine(engine);
-
-        [HttpGet]
-        [Route("api/Cars/{engine}/CitiesForEngine")]
-        public IEnumerable<string> GetCitiesForEngine(EngineType engine) => carDAO.GetCitiesForEngine(engine);
-
-        //---------- year selected ----------
-        [HttpGet]
-        [Route("api/Cars/{year}/BrandsForYear")]
-        public IEnumerable<string> GetBrandsForYear(int year) => carDAO.GetBrandsForYear(year);
-
-        [HttpGet]
-        [Route("api/Cars/{year}/ModelsForYear")]
-        public IEnumerable<string> GetModelsForEngine(int year) => carDAO.GetModelsForYear(year);
-
-        [HttpGet]
-        [Route("api/Cars/{year}/EnginesForYear")]
-        public IEnumerable<EngineType> GetEnginesForYear(int year) => carDAO.GetEnginesForYear(year);
-
-        [HttpGet]
-        [Route("api/Cars/{year}/CitiesForYear")]
-        public IEnumerable<string> GetCitiesForEngine(int year) => carDAO.GetCitiesForYear(year);
-
-        //---------- year selected ----------
-        //[HttpGet]
-        //[Route("api/Cars/{city}/BrandsForCity")]
-        //public IEnumerable<string> GetBrandsForCity(string city) => carDAO.GetBrandsForCity(city);
-
-        //[HttpGet]
-        //[Route("api/Cars/{city}/ModelsForCity")]
-        //public IEnumerable<string> GetModelsForCity(string city) => carDAO.GetModelsForCity(city);
-
-        //[HttpGet]
-        //[Route("api/Cars/{city}/EnginesForCity")]
-        //public IEnumerable<EngineType> GetEnginesForCity(string city) => carDAO.GetEnginesForCity(city);
-
-        //[HttpGet]
-        //[Route("api/Cars/{city}/YearsForCity")]
-        //public IEnumerable<int> GetYearsForCity(string city) => carDAO.GetYearsForCity(city);
+            return cars;
+        }
     }
 }
